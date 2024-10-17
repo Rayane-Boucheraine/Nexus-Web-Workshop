c0d415ef63eff3899adaa2f4b8a61c43768c1c09
Rayane Boucheraine <rayaneha08@gmail.com>
2024-10-17 13:43:49 +0100

    add first page

--- client/src/app/SignIn/page.tsx
+++ client/src/app/SignIn/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import vector from "../../public/vector.svg";
+import vector from "../../../public/vector.svg";
 import Link from "next/link";
 
-export default function Home() {
+const page = () => {
   return (
     <div className="relative flex items-center justify-center w-[100vw] h-[100vh] overflow-hidden bg-gradient-to-b from-[#232323] to-[#181717]">
       <Image
@@ -11,9 +11,11 @@ export default function Home() {
         className="absolute right-0 top-0 w-[100%]"
       />
       <div className="flex gap-6 flex-col justify-center items-center h-[300px] p-[20px] w-[40%] z-[50] rounded-[24.369px] bg-white/80 backdrop-blur-[calc(var(--sds-size-depth-400)_/_2)">
-        <h2 className="text-[40px] font-[600]">Welcome To Our Club</h2>
-        <Link href="/SignUp" className="font-[700] text-[22px]">Start</Link>
+        <h2>Welcome To Our Club</h2>
+        <Link href="/SignUp">Start</Link>
       </div>
     </div>
   );
-}
+};
+
+export default page;
