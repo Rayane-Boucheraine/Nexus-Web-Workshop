92a8ebd8b658c50d0174cdaa1bf7022a3e43edc5
Rayane Boucheraine <rayaneha08@gmail.com>
2024-10-19 09:49:10 +0100

    change baseUrl link

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: 'https://nexus-web-workshop.vercel.app',
     methods: 'GET,POST,PUT,PATCH,DELETE',
-    credentials: true, 
+    credentials: true,
   });
 
   app.useGlobalPipes(
